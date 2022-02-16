474b09d65ccba27301bd44a9b2acfca24b343f43
Rico-la <rico.alves91@gmail.com>
2022-02-16 15:55:30 +0100

    add camera flip and take a picture only metada in log

--- screen/CameraScreen.js
+++ screen/CameraScreen.js
@@ -1,6 +1,52 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { Text, View, Button } from 'react-native'
+import { Camera } from 'expo-camera';
+import * as ImageManipulator from "expo-image-manipulator";
 
 export default function CameraScreen() {
-  return <Text>Camera Screen</Text>
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const cameraRef = useRef(null);
+  
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  
+  return (
+    <>
+      <Camera style={{ flex: 1 }} ref={cameraRef} type={type}/>
+      <Button
+        title='Flip'
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}>
+      </Button>
+      <Button 
+        title='Take a picture' 
+        onPress={async () => {
+          const pictureMetadata = await cameraRef.current.takePictureAsync();
+          console.log("pictureMetadata", pictureMetadata);
+          console.log(
+            await ImageManipulator.manipulateAsync(pictureMetadata.uri, [
+              { resize: { width: 800 } },
+            ])
+          );
+        }} 
+      />
+    </>
+  );
 }
