4f4f2ff2f484ef6a1f5a2022405abe4c36735194
Rico-la <rico.alves91@gmail.com>
2022-02-16 17:17:48 +0100

    feed screen

--- screen/FeedScreen.js
+++ screen/FeedScreen.js
@@ -1,6 +1,42 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Image, FlatList, StyleSheet } from "react-native";
 
 export default function FeedScreen() {
-  return <Text>Feed Screen</Text>
+  const [serverImagesUrls, setServerImagesUrls] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const filesUrl = await axios.get(
+        "https://wildstagram.nausicaa.wilders.dev/list"
+      );
+      setServerImagesUrls(filesUrl.data);
+    })();
+  }, []);
+  return serverImagesUrls.length > 0 ? (
+    <FlatList
+      data={serverImagesUrls}
+      keyExtractor={(serverImageURI) => serverImageURI}
+      renderItem={(itemData) => {
+        return (
+          <>
+            <Image
+              style={styles.image}
+              source={{
+                uri:
+                  "https://wildstagram.nausicaa.wilders.dev/files/" +
+                  itemData.item,
+              }}
+            />
+          </>
+        );
+      }}
+    />
+  ) : null;
 }
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: "contain",
+    height: 500,
+  },
+});
